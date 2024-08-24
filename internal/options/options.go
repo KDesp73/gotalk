2225636e22c855a1ba0230b73a312a9b609e6354
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-08-24 15:13:34 +0300

    refactored option handling

---
+++ internal/options/options.go
@@ -0,0 +1,41 @@
+package options
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"gotalk/api/state"
+	"gotalk/internal/encryption"
+	"os"
+)
+
+type Options struct {
+	Port int
+	GenerateKey bool
+}
+
+func ParseOptions() *Options {
+	var port = 8080
+	var generate_key = false
+	flag.IntVar(&port, "port", port, "Specify the port")
+	flag.BoolVar(&generate_key, "generate-key", generate_key, "Generate an encryption key")
+	flag.Parse()
+
+	return &Options{
+		Port: port,
+		GenerateKey: generate_key,
+	}
+}
+
+// Handles all cases where the program needs to exit
+func (o *Options)HandleOptions() {
+	if o.GenerateKey {
+		key, err := encryption.GenerateKey(state.KeySize)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERRO: Could not generate key\n")
+			os.Exit(1)
+		}
+		fmt.Printf("Key: %s\n", hex.EncodeToString(key))
+		os.Exit(0)
+	}
+}
