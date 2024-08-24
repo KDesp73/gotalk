3ae926e71ef60ae2a8a5deb10072fd0f6e75ef80
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-08-24 03:42:41 +0300

    Added error json responses

---
+++ api/v1/errors/errors.go
@@ -0,0 +1,21 @@
+package errors
+
+import (
+	"fmt"
+	"gotalk/internal/json"
+	"net/http"
+)
+
+var STATUS_CODE = http.StatusBadRequest
+
+var PARSING_FORM_FAILED = json.Json{
+	Status: STATUS_CODE,
+	Message: "Unable to parse form",
+}
+
+var NOT_FOUND = func(s string) json.Json {
+	return json.Json{
+		Status: STATUS_CODE,
+		Message: fmt.Sprintf("%s not found", s),
+	}
+}
